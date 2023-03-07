c9ee1083c1166b1f7226395e708848a4648c6eaa
Zachary Honey <zhoney466@gmail.com>
2023-03-07 08:35:12 -0700

    Added Theme.js

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,20 @@
 // Write your Character component here
+import React from 'react';
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import "./App.css";
+
+
+
+
+function Character(props) {
+    return (
+        <div className='container'>
+        
+
+        </div>
+    )
+}
+
+export default Character;
--- src/index.js
+++ src/index.js
@@ -4,8 +4,15 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import "./App.css";
+import theme from './Theme';
+
+import { ThemeProvider } from "styled-components";
 
 import { worker } from "./mocks/browser";
 worker.start();
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+    <ThemeProvider theme={theme}>
+        <App />
+    </ThemeProvider>,
+document.getElementById("root"));
