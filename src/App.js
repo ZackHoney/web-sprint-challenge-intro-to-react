0ff157c5ccd86a580164b0ae5aacd693562ea59a
Zachary Honey <zhoney466@gmail.com>
2023-03-07 10:35:48 -0700

    Worked on sprint challenge

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ function App() {
   useEffect(() => {
     axios.get("https://swapi.dev/api/people/")
     .then(res => {
+      console.log(res.data);
       setData(res.data);
     })
     .catch(err => console.error(err))
